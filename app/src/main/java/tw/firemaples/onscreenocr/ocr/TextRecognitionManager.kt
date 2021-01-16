3fbbb049b002098bf95e5119201321343f27fbdb
firemaples <firemaples@gmail.com>
2021-01-17 00:45:39 +0800

    temporary commit

--- app/src/main/java/tw/firemaples/onscreenocr/ocr/TextRecognitionManager.kt
+++ app/src/main/java/tw/firemaples/onscreenocr/ocr/TextRecognitionManager.kt
@@ -1,9 +1,28 @@
 package tw.firemaples.onscreenocr.ocr
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Rect
+import tw.firemaples.onscreenocr.ocr.tesseract.OcrResult
+import tw.firemaples.onscreenocr.utils.ImageFile
 import java.io.File
 
 object TextRecognitionManager {
-    fun recognizeText(imageFile: File) {
+    fun recognize(imageFile: ImageFile, rect: Rect) {
+        val fullBitmap = BitmapFactory.decodeFile(imageFile.file.absolutePath)
+        val croppedBitmap = Bitmap.createBitmap(fullBitmap, rect.left, rect.top, rect.width(), rect.height())
 
+
+    }
+
+    interface ITextRecognitionEngine {
+        fun recognize(bitmap: Bitmap)
+    }
+
+    interface TextRecognitionCallback {
+        fun initializing()
+        fun initialized()
+        fun onRecognizing()
+        fun onRecognized(ocrResult: OcrResult)
     }
 }
---
+++ app/src/main/java/tw/firemaples/onscreenocr/ocr/tesseract/TesseractTextRecognitionEngine.kt
@@ -0,0 +1,10 @@
+package tw.firemaples.onscreenocr.ocr.tesseract
+
+import android.graphics.Bitmap
+import tw.firemaples.onscreenocr.ocr.TextRecognitionManager
+
+object TesseractTextRecognitionEngine : TextRecognitionManager.ITextRecognitionEngine {
+    override fun recognize(bitmap: Bitmap) {
+
+    }
+}
