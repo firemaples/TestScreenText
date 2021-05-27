4903ac6069248378ae001594658e96ed904b5ec4
firemaples <firemaples@gmail.com>
2021-05-27 22:54:47 +0800

    temp

--- refactor/src/main/java/tw/firemaples/onscreenocr/ui/permissions/PermissionCaptureScreenFragment.kt
+++ refactor/src/main/java/tw/firemaples/onscreenocr/ui/permissions/PermissionCaptureScreenFragment.kt
@@ -1,8 +1,13 @@
 package tw.firemaples.onscreenocr.ui.permissions
 
+import android.app.Activity
+import android.content.Context
+import android.media.projection.MediaProjectionManager
 import android.os.Bundle
 import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
 import tw.firemaples.onscreenocr.R
+import tw.firemaples.onscreenocr.screenshot.ScreenshotManager
 import tw.firemaples.onscreenocr.ui.BaseFragment
 import kotlin.reflect.KClass
 
@@ -16,6 +21,28 @@ class PermissionCaptureScreenFragment : BaseFragment<PermissionCaptureScreenView
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        if (ScreenshotManager.isGranted) {
+            startService()
+        } else {
+            requestMediaProject()
+        }
+    }
+
+    private fun requestMediaProject() {
+        val manager = requireContext().getSystemService(Context.MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
+        resultLauncher.launch(manager.createScreenCaptureIntent())
+    }
+
+    private val resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        val intent = it.data
+        if (it.resultCode == Activity.RESULT_OK && intent != null) {
+            ScreenshotManager.onMediaProjectionGranted(intent)
+            startService()
+        }
+    }
+
+    private fun startService() {
 
     }
 }
